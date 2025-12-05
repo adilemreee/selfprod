3acb5d4d89244d867350e23361054b9ba3444e7a
adilemreee <adil.emre@hotmail.com>
2025-12-05 20:32:29 +0300

    Initial Commit

---
+++ selfprod Watch App/selfprodApp.swift
@@ -0,0 +1,17 @@
+//
+//  selfprodApp.swift
+//  selfprod Watch App
+//
+//  Created by adil emre  on 5.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct selfprod_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
